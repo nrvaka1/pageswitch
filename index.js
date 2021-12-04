93b1ecb66f0fdc18a8a1d54087c1fd00c424c720
LAPTOP-69R06ILF\nrvak <nrvaka2@gmail.com>
2021-12-04 20:29:14 +0100

    correct4

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.use('/api', datp);
 
 // start server
 
-const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+//const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 
-//const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 //const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 
-/*const server = app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
     console.log('Server listening on port ' + PORT);
-});*/
-app.listen(PORT);
+});
+//app.listen(PORT);
