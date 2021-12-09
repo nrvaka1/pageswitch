5f187fd68bfe23bf6fe70999584224e606320304
LAPTOP-69R06ILF\nrvak <nrvaka2@gmail.com>
2021-12-09 10:43:57 +0100

    Change ID4

--- routes/api/dat.js
+++ routes/api/dat.js
@@ -15,7 +15,7 @@ router.post('/print', async (req, res) => {
 router.get('/print1', async (req, res) => {
     let database = new Database(false);
     console.log(' print1: abncnd ');
-    database.nameret(1, results => {
+    database.nameret(4, results => {
         //  console.log(' Order ID Server : ' + results.valid);
         console.log(" Data from Client : " + results[0].fname)
         res.json(results);
